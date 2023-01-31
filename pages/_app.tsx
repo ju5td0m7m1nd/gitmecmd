7082caa49c9df573fa251386163dc3d7f6b46c7d
ju5td0m7m1nd <franks0059@gmail.com>
2023-01-31 23:11:00 +0800

    chore: add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from '@chakra-ui/react'
+import ReactGA from 'react-ga4';
 
 import "../styles/globals.css";
+
+ReactGA.initialize('G-D2TLX39XJS');
+ReactGA.send('pageview');
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
